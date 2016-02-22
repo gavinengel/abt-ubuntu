e38f8e2ab47865da8f59230aef4268c274e5798f
Gavin Engel <gavin@engel.com>
2016-02-21 19:33:34 -0500

    Update update.php

--- update.php
+++ update.php
@@ -1,5 +1,31 @@
-#!/bin/env php
+#!/usr/bin/php
 <?php
 
+/**
+ * declarations
+ */
+$path = dirname(__FILE__) . '/';
+$manifest = json_decode(file_get_contents ($path . "manifest.json"), true);
+require ($path . "functions.php");
 
+/**
+ * update this git repository
+ */
+echo "Updating $path:\n";
+echo `cd $path && git pull && cd -;`;
+echo "\n\n";
 
+/**
+ * perform apt-get commands
+ */
+foreach ($manifest['apt-get'] as $cmd => $pkgs) {
+  $pkg = implode(' ', $pkgs);
+  $output = aptGet($cmd, $pkg);
+  $info = [
+    'cmd' => "apt-get $cmd $pkg",
+    'output' => $output
+  ];
+  
+  logger ($info);
+
+}
